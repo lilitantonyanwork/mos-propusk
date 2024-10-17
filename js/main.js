587a6d8c5b0b8f00da2ee5099b89ae70ced9ad24
lilitantonyan <lilit@helix.am>
2024-10-17 18:38:37 +0400

    add checkboxes style

--- js/main.js
+++ js/main.js
@@ -13,10 +13,48 @@ for (i = 0; i < acc.length; i++) {
     });
 }
 
-var swiper = new Swiper(".client-list", {
-    slidesPerView: 4,
-    spaceBetween: 20,
-    autoplay: {
-        delay: 5000,
-    },
-});
+var init = false;
+var swiper1;
+function swiperCard(item) {
+
+    if (window.innerWidth <= 990) {
+        console.log(init)
+
+        if (!init) {
+            console.log(item)
+            init = true;
+            swiper1 = new Swiper(item, {
+                slidesPerView: 2,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+                breakpoints: {
+
+                    480: {
+                        slidesPerView: 1,
+
+                    },
+                    600: {
+                        slidesPerView: 2,
+
+                    },
+
+                },
+            });
+            init = false;
+        }
+    } else if (init) {
+        swiper.destroy();
+        init = false;
+    }
+}
+swiperCard('.help-list');
+swiperCard('.price-list');
+swiperCard('.review-list');
+swiperCard('.news-list');
+window.addEventListener("resize", swiperCard('.help-list'));
+window.addEventListener("resize", swiperCard('.price-list'));
+window.addEventListener("resize", swiperCard('.review-list'));
+
+window.addEventListener("resize", swiperCard('.news-list'));
